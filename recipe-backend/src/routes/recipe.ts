7da4a142f7cffe22fb7224e930c1d6e960725b2e
Mahmoud <mmhafez51@gmail.com>
2022-04-30 19:27:34 +0200

    create recipe details view (edit/delete)

--- recipe-backend/src/routes/recipe.ts
+++ recipe-backend/src/routes/recipe.ts
@@ -62,10 +62,15 @@ const create = async (req: Request, res: Response) => {
 
 const update = async (req: Request, res: Response) => {
   try {
+    const img = req.file;
+    const recipe = {
+      ...JSON.parse(req.body.data),
+      imgName: img ? img.filename : null,
+    };
     const result = await Recipe.updateOne(
       { _id: req.params.id },
       {
-        $set: req.body,
+        $set: recipe,
       }
     );
     res.json(result);
@@ -87,7 +92,7 @@ router.get("/", getAll);
 router.get("/:id", getOne);
 router.get("/image/:imgName", getImage);
 router.post("/", upload.single("image"), create);
-router.patch("/:id", update);
+router.put("/:id", upload.single("image"), update);
 router.delete("/:id", remove);
 
 export default router;
